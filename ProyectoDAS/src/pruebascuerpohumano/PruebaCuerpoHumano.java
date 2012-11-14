4942c5807aface4a5351b4b456635fdd97ecbf36
andreacorrea <andynice@hotmail.com>
2012-11-14 12:28:00 -0600

    Se agrega la clase Scene.java encargada de manejar el GeometricFiguresManager y UsersManager.

--- ProyectoDAS/src/pruebascuerpohumano/PruebaCuerpoHumano.java
+++ ProyectoDAS/src/pruebascuerpohumano/PruebaCuerpoHumano.java
@@ -12,20 +12,15 @@ import processing.core.*;
 
 public class PruebaCuerpoHumano extends PApplet {
 
-    SimpleOpenNI context;
-// this object deals with the user callbacks
-    UsersManager usersManager;
+    Scene scene;
+
     User currentUser;
     float rotX;  // by default rotate the hole scene 180deg around the x-axis, 
     // the data from openni comes upside down
     float rotY;
-    Torso torso1;
-    float offset = 50;
-    PVector torso1_LeftUpper;
-    PVector torso1_RightUpper;
-    PVector torso1_LeftLower;
-    PVector torso1_RightLower;
-
+    
+    SimpleOpenNI context;
+    
     @Override
     public void setup() {
         // create a window the size of the scene
@@ -34,33 +29,15 @@ public class PruebaCuerpoHumano extends PApplet {
 
         // instantiate a new context
         frameRate(24);
-
-        setupTorsos();
-
+        
         context = new SimpleOpenNI(this);
-
-        // enable depthMap generation 
-        if (context.enableDepth() == false) {
-            println("Can't open the depthMap, maybe the camera is not connected!");
-            exit();
-            return;
-        }
-
-        // enable scene analyser
-        if (context.enableScene() == false) {
-            println("Can't setup scene");
-            exit();
-            return;
-        }
-
+        
+        scene = new Scene(this);
+        
         // disable mirror
         context.setMirror(true);
 
-        // setup the callback helper class
-        usersManager = new UsersManager(this, context);
-
-        // enable skeleton generation for all joints, direct all callback to the helper class
-        context.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL, usersManager);
+        scene.activateUsersManager(context);
 
     }
 
@@ -91,44 +68,27 @@ public class PruebaCuerpoHumano extends PApplet {
         line(width / 2, height / 2, 200, width / 2, height / 2, -200);
         popStyle();
 
-        if (usersManager.getUsers().size() > 0) {
+        if (scene.getUsersManager().getUsersSize()> 0) {
 
             // draw the skeleton of user1
-            Iterator user = usersManager.getUsers().values().iterator();
+            Iterator user = scene.getUsersManager().getUsers().values().iterator();
 
             while (user.hasNext()) {
                 currentUser = ((User) user.next());
                 //currentUser.getBody().drawSkeletonLines();
                 //currentUser.getBody().drawJoints();
                 currentUser.getBody().update();
-                currentUser.getBody().circleForAHead();
+                //currentUser.getBody().circleForAHead();
             }
         } else {
 
             // draw scene Image
             image(context.sceneImage(), 0, 0);
         }
-        //rpuebaTorsos();
+        
     }
 
-    public void setupTorsos() {
-        //Torso1
-        torso1_LeftUpper = new PVector(width / 2 - offset, height / 2 - offset, 0);
-        torso1_RightUpper = new PVector(width / 2 + offset, height / 2 - offset, -300);
-        torso1_LeftLower = new PVector(width / 2 - offset, height / 2 + offset, 0);
-        torso1_RightLower = new PVector(width / 2 + offset, height / 2 + offset, 0);
 
-        torso1 = new Torso(this, "torso1", color(255, 0, 0));
-    }
-
-    public void pruebaTorsos() {
-        point(torso1_LeftUpper.x, torso1_LeftUpper.y, 0);
-        point(torso1_RightUpper.x, torso1_RightUpper.y, torso1_RightUpper.z);
-        point(torso1_LeftLower.x, torso1_LeftLower.y, 0);
-        point(torso1_RightLower.x, torso1_RightLower.y, 0);
-        torso1.update(torso1_LeftUpper, torso1_RightUpper, torso1_LeftLower, torso1_RightLower);
-        torso1.drawBodyMember();
-    }
 
     @Override
     public void keyPressed() {
---
+++ ProyectoDAS/src/pruebascuerpohumano/Scene.java
@@ -0,0 +1,58 @@
+package pruebascuerpohumano;
+
+import SimpleOpenNI.SimpleOpenNI;
+import processing.core.PApplet;
+
+public class Scene {
+    
+    PApplet pApplet;
+    SimpleOpenNI context;
+    
+    
+    // this object deals with the user callbacks
+    UsersManager usersManager;
+    GeometricFiguresManager geometricFiguresManager;
+    
+    public Scene(PApplet pApplet){
+        this.pApplet = pApplet;
+    }
+    
+    public void activateUsersManager(SimpleOpenNI context){
+        this.context = context;
+        // setup the callback helper class
+        usersManager = new UsersManager(pApplet, context);
+        // enable depthMap generation 
+        if (context.enableDepth() == false) {
+            pApplet.println("Can't open the depthMap, maybe the camera is not connected!");
+            pApplet.exit();
+            return;
+        }
+
+        // enable scene analyser
+        if (context.enableScene() == false) {
+            pApplet.println("Can't setup scene");
+            pApplet.exit();
+            return;
+        }
+        
+        // enable skeleton generation for all joints, direct all callback to the helper class
+        context.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL, usersManager);
+        
+        
+        
+        
+    }
+    
+    public void activateGeometricFiguresManager(SimpleOpenNI context){
+        geometricFiguresManager = new GeometricFiguresManager();
+    }
+
+    public UsersManager getUsersManager() {
+        return usersManager;
+    }
+
+    public GeometricFiguresManager getGeometricFiguresManager() {
+        return geometricFiguresManager;
+    }
+
+}
